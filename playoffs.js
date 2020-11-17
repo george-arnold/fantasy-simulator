6014d1a9ec82313783858334b1b88ce064f2f8dc
George Arnold <george.arnold.88@gmail.com>
2020-11-16 22:23:52 -0500

    made files

---
+++ playoffs.js
@@ -0,0 +1,65 @@
+// total number of playoff visits through simuluation
+let PLAYOFFS = [
+    {
+      playerName: 'Matt',
+      playoffCount: 0,
+      charlotteCount:0
+    },
+    {
+      playerName: 'Kevin',
+      playoffCount: 0,
+      charlotteCount:0
+    },
+    {
+      playerName: 'Kate',
+      playoffCount: 0,
+      charlotteCount:0
+    },
+   {
+     playerName: 'Johnny',
+     playoffCount: 0,
+     charlotteCount:0
+   }, 
+   {
+    playerName: 'Kaitlin',
+    playoffCount: 0,
+    charlotteCount:0
+  }, 
+  {
+    playerName: 'Sean',
+    playoffCount: 0,
+    charlotteCount:0
+  }, 
+  {
+    playerName: 'Shannon',
+    playoffCount: 0,
+    charlotteCount:0
+  }, 
+  {
+    playerName: 'Meg',
+    playoffCount: 0,
+    charlotteCount:0
+  }, 
+  {
+    playerName: 'David',
+    playoffCount: 0,
+    charlotteCount:0
+  }, 
+  {
+    playerName: 'Mike',
+    playoffCount: 0,
+    charlotteCount:0
+  }, 
+  {
+    playerName: 'Karen',
+    playoffCount: 0,
+    charlotteCount:0
+  }, 
+  {
+    playerName: 'Colleen',
+    playoffCount: 0,
+    charlotteCount:0
+  }, 
+  ];
+
+  export default PLAYOFFS;
---
+++ schedules.js
@@ -0,0 +1,119 @@
+//create schedules for each players
+export const matchesWeek11 = [
+    {
+     
+      playerOne:'Kate',
+      playerTwo:'Matt'
+    }, 
+    {
+     
+      playerOne:'Kevin',
+      playerTwo:'Karen'
+    }, 
+    {
+      playerOne:'Sean',
+      playerTwo:'David'
+    }, 
+    {
+      playerOne:'Kaitlin',
+      playerTwo:'Colleen'
+    }, 
+    {
+      playerOne:'Shannon',
+      playerTwo:'Mike'
+    }, 
+    {
+      playerOne:'Johnny',
+      playerTwo:'Meg'
+    }, 
+  
+  ];
+  
+  // week 12
+  export const matchesWeek12 = [
+    {
+     
+      playerOne:'Sean',
+      playerTwo:'Matt'
+    }, 
+    {
+     
+      playerOne:'Kevin',
+      playerTwo:'Kaitlin'
+    }, 
+    {
+      playerOne:'David',
+      playerTwo:'Kate'
+    }, 
+    {
+      playerOne:'Colleen',
+      playerTwo:'Karen'
+    }, 
+    {
+      playerOne:'Shannon',
+      playerTwo:'Johnny'
+    }, 
+    {
+      playerOne:'Meg',
+      playerTwo:'Mike'
+    }, 
+  
+  ];
+  // week 13
+  export const matchesWeek13 = [
+    {
+     
+      playerOne:'Johnny',
+      playerTwo:'Matt'
+    }, 
+    {
+     
+      playerOne:'Kevin',
+      playerTwo:'Mike'
+    }, 
+    {
+      playerOne:'Sean',
+      playerTwo:'Shannon'
+    }, 
+    {
+      playerOne:'David',
+      playerTwo:'Colleen'
+    }, 
+    {
+      playerOne:'Kaitlin',
+      playerTwo:'Meg'
+    }, 
+    {
+      playerOne:'Karen',
+      playerTwo:'Kate'
+    }, 
+  
+  ];
+  // week 14
+  export const matchesWeek14 = [
+    {
+      playerOne:'Shannon',
+      playerTwo:'Matt'
+    }, 
+    {
+      playerOne:'Kevin',
+      playerTwo:'Meg'
+    }, 
+    {
+      playerOne:'Sean',
+      playerTwo:'Johnny'
+    }, 
+    {
+      playerOne:'David',
+      playerTwo:'Karen'
+    }, 
+    {
+      playerOne:'Kaitlin',
+      playerTwo:'Mike'
+    }, 
+    {
+      playerOne:'Colleen',
+      playerTwo:'Kate'
+    }, 
+  
+  ];
